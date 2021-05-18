6caea9828fea0f52640326293af3fb16fa550639
paharip <partha.pahari@gmail.com>
2021-05-18 17:01:34 +0530

    refactor the singleton

---
+++ singleton/singleton_test.go
@@ -0,0 +1,23 @@
+package singleton
+
+import "testing"
+
+func TestGetInstance(t *testing.T) {
+	counter1 := GetInstance()
+	if counter1 == nil {
+		t.Error("expected pointer to singleton after calling GetInstance(), not nil")
+	}
+	expectedCounter := counter1
+
+	currentCount := counter1.AddOne()
+	if currentCount != 1 {
+		t.Errorf("After calling for the first time to count, the count must be 1 but it is %d\n", currentCount)
+	}
+
+	counter2 := GetInstance()
+	if counter2 != expectedCounter {
+		//Test 2 failed
+		t.Error("Expected same instance in counter2 but it got a different instance")
+	}
+
+}
